730911a78e3ebd9630010ce52874a375646dde20
StPaulis <shamos_pv@hotmail.com>
2020-09-14 14:48:27 +0000

    exp: update run.sh for single org

--- run.sh
+++ run.sh
@@ -20,47 +20,47 @@ echo "# ------------------------------------------------------------------------
 docker exec "$CLI_NAME" peer channel join -b "$CHANNEL_NAME".block --tls --cafile $ORDERER_CA_LOCATION
 sleep 2
 
-echo 
-echo "# ---------------------------------------------------------------------------"
-echo "# Join channel : peer1.org1.example.com"
-echo "# ---------------------------------------------------------------------------"
-docker exec \
-	-e "CORE_PEER_LOCALMSPID=Org1MSP" \
-	-e "CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.crt" \
-	-e "CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.key" \
-	-e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt" \
-	-e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" \
-	-e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" \
-	"$CLI_NAME" peer channel join -b "$CHANNEL_NAME".block --tls --cafile $ORDERER_CA_LOCATION
-sleep 2
+# echo 
+# echo "# ---------------------------------------------------------------------------"
+# echo "# Join channel : peer1.org1.example.com"
+# echo "# ---------------------------------------------------------------------------"
+# docker exec \
+# 	-e "CORE_PEER_LOCALMSPID=Org1MSP" \
+# 	-e "CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.crt" \
+# 	-e "CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.key" \
+# 	-e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt" \
+# 	-e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" \
+# 	-e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" \
+# 	"$CLI_NAME" peer channel join -b "$CHANNEL_NAME".block --tls --cafile $ORDERER_CA_LOCATION
+# sleep 2
 
-echo 
-echo "# ---------------------------------------------------------------------------"
-echo "# Join channel : peer0.org2.example.com"
-echo "# ---------------------------------------------------------------------------"
-docker exec \
-	-e "CORE_PEER_LOCALMSPID=Org2MSP" \
-	-e "CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/server.crt" \
-	-e "CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/server.key" \
-	-e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
-	-e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" \
-	-e "CORE_PEER_ADDRESS=peer0.org2.example.com:7051" \
-	"$CLI_NAME" peer channel join -b "$CHANNEL_NAME".block --tls --cafile $ORDERER_CA_LOCATION
-sleep 2
+# echo 
+# echo "# ---------------------------------------------------------------------------"
+# echo "# Join channel : peer0.org2.example.com"
+# echo "# ---------------------------------------------------------------------------"
+# docker exec \
+# 	-e "CORE_PEER_LOCALMSPID=Org2MSP" \
+# 	-e "CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/server.crt" \
+# 	-e "CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/server.key" \
+# 	-e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
+# 	-e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" \
+# 	-e "CORE_PEER_ADDRESS=peer0.org2.example.com:7051" \
+# 	"$CLI_NAME" peer channel join -b "$CHANNEL_NAME".block --tls --cafile $ORDERER_CA_LOCATION
+# sleep 2
 
-echo 
-echo "# ---------------------------------------------------------------------------"
-echo "# Join channel : peer1.org2.example.com"
-echo "# ---------------------------------------------------------------------------"
-docker exec \
-	-e "CORE_PEER_LOCALMSPID=Org2MSP" \
-	-e "CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/server.crt" \
-	-e "CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/server.key" \
-	-e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/ca.crt" \
-	-e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" \
-	-e "CORE_PEER_ADDRESS=peer1.org2.example.com:7051" \
-	"$CLI_NAME" peer channel join -b "$CHANNEL_NAME".block --tls --cafile $ORDERER_CA_LOCATION
-sleep 2
+# echo 
+# echo "# ---------------------------------------------------------------------------"
+# echo "# Join channel : peer1.org2.example.com"
+# echo "# ---------------------------------------------------------------------------"
+# docker exec \
+# 	-e "CORE_PEER_LOCALMSPID=Org2MSP" \
+# 	-e "CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/server.crt" \
+# 	-e "CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/server.key" \
+# 	-e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/ca.crt" \
+# 	-e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" \
+# 	-e "CORE_PEER_ADDRESS=peer1.org2.example.com:7051" \
+# 	"$CLI_NAME" peer channel join -b "$CHANNEL_NAME".block --tls --cafile $ORDERER_CA_LOCATION
+# sleep 2
 
 echo 
 echo "# ---------------------------------------------------------------------------"
@@ -72,16 +72,16 @@ sleep 2
 echo 
 echo "# ---------------------------------------------------------------------------"
 echo "# Update anchor peer : Org2"
-echo "# ---------------------------------------------------------------------------"
-docker exec \
-	-e "CORE_PEER_LOCALMSPID=Org2MSP" \
-	-e "CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/server.crt" \
-	-e "CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/server.key" \
-	-e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
-	-e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" \
-	-e "CORE_PEER_ADDRESS=peer0.org2.example.com:7051" \
-	"$CLI_NAME" peer channel update  -o "$ORDERER_NAME":7050 -c "$CHANNEL_NAME" -f "$ORG2_ANCHOR_TX" --tls --cafile $ORDERER_CA_LOCATION
-sleep 2
+# echo "# ---------------------------------------------------------------------------"
+# docker exec \
+# 	-e "CORE_PEER_LOCALMSPID=Org2MSP" \
+# 	-e "CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/server.crt" \
+# 	-e "CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/server.key" \
+# 	-e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
+# 	-e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" \
+# 	-e "CORE_PEER_ADDRESS=peer0.org2.example.com:7051" \
+# 	"$CLI_NAME" peer channel update  -o "$ORDERER_NAME":7050 -c "$CHANNEL_NAME" -f "$ORG2_ANCHOR_TX" --tls --cafile $ORDERER_CA_LOCATION
+# sleep 2
 
 echo 
 echo "# ---------------------------------------------------------------------------"
@@ -90,19 +90,19 @@ echo "# ------------------------------------------------------------------------
 docker exec "$CLI_NAME" peer chaincode install -n "$CHAINCODE_NAME" -p "$CHAINCODE_SRC" -v $CHAINCODE_VERSION
 sleep 2
 
-echo 
-echo "# ---------------------------------------------------------------------------"
-echo "# Installing chaincode on org2"
-echo "# ---------------------------------------------------------------------------"
-docker exec \
-	-e "CORE_PEER_LOCALMSPID=Org2MSP" \
-	-e "CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/server.crt" \
-	-e "CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/server.key" \
-	-e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
-	-e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" \
-	-e "CORE_PEER_ADDRESS=peer0.org2.example.com:7051" \
-	"$CLI_NAME" peer chaincode install -n "$CHAINCODE_NAME" -p "$CHAINCODE_SRC" -v $CHAINCODE_VERSION
-sleep 2
+# echo 
+# echo "# ---------------------------------------------------------------------------"
+# echo "# Installing chaincode on org2"
+# echo "# ---------------------------------------------------------------------------"
+# docker exec \
+# 	-e "CORE_PEER_LOCALMSPID=Org2MSP" \
+# 	-e "CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/server.crt" \
+# 	-e "CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/server.key" \
+# 	-e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
+# 	-e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" \
+# 	-e "CORE_PEER_ADDRESS=peer0.org2.example.com:7051" \
+# 	"$CLI_NAME" peer chaincode install -n "$CHAINCODE_NAME" -p "$CHAINCODE_SRC" -v $CHAINCODE_VERSION
+# sleep 2
 
 echo 
 echo "# ---------------------------------------------------------------------------"
